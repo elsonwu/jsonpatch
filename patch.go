44c8dcccb3acb961e56cbbd614de006a44431ccc
elsonwu <elsonwu@126.com>
2014-08-02 17:56:15 +0800

    add missing file

---
+++ patch.go
@@ -0,0 +1,23 @@
+package jsonpatch
+
+import "encoding/json"
+
+func Run(jsonPos string, model interface{}) error {
+	ops := []Patch{}
+	if err := json.Unmarshal([]byte(jsonPos), &ops); err != nil {
+		return err
+	}
+
+	for _, opt := range ops {
+		f, err := FindField(model, opt)
+		if err != nil {
+			return err
+		}
+
+		if err := Do(f, opt); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
